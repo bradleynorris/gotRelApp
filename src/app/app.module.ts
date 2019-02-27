46b04e3000f7604a319edd2142844eaf8bbe1f85
bradleynorris <risebaxelrod@gmail.com>
2019-02-27 15:03:33 -0800

    added components and class/interface files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SelectComponent } from './select/select.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SelectComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-results.ts
@@ -0,0 +1,5 @@
+import { Result } from './result';
+
+export const RESULTS: Result[] = [
+    {id: '', title: '', description: ''},
+]
